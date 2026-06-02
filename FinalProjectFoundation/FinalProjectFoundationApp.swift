d3fdcb1535212af3b413d7cb8b004797c229563f
Clara Vas <vasconcelosousa2508@gmail.com>
2026-06-02 14:11:45 -0300

    Initial commit

---
+++ FinalProjectFoundation/FinalProjectFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectFoundationApp.swift
+//  FinalProjectFoundation
+//
+//  Created by Beatriz Leonel on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ FinalProjectFoundation/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinalProjectFoundation
+//
+//  Created by Beatriz Leonel on 28/05/26.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
