68391c72a12ec236892399a9561aed31afbf057f
Clara <vasconcelosousa2508@gmail.com>
2026-06-09 17:52:52 -0300

    Ajuste na BibliotacaView e adição de nova view

---
+++ FinalProjectFoundation/BiblioAgeView.swift
@@ -0,0 +1,18 @@
+//
+//  BiblioAgeView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 09/06/26.
+//
+
+import SwiftUI
+
+struct BiblioAgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BiblioAgeView()
+}
---
+++ FinalProjectFoundation/DesingSystem.swift
@@ -0,0 +1,18 @@
+//
+//  DesingSystem.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 09/06/26.
+//
+
+import SwiftUI
+
+struct DesingSystem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DesingSystem()
+}
