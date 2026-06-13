f58b38835b455d45af54f193287787cc71e84dca
Clara <vasconcelosousa2508@gmail.com>
2026-06-13 02:43:53 -0300

    testes com bd

---
+++ FinalProjectFoundation/DetalheLivroView.swift
@@ -0,0 +1,18 @@
+//
+//  DetalheLivroView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 13/06/26.
+//
+
+import SwiftUI
+
+struct DetalheLivroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetalheLivroView()
+}
---
+++ FinalProjectFoundation/Models.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 13/06/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
