8e725a3a4bdaaba666fcfb53c4f85d523f711620
Clara <vasconcelosousa2508@gmail.com>
2026-06-16 23:42:13 -0300

    testes p perfil, selectAvatar e tentativa de SelectPerfil

---
+++ FinalProjectFoundation/SelectPerfilView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectPerfilView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 16/06/26.
+//
+
+import SwiftUI
+
+struct SelectPerfilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectPerfilView()
+}
