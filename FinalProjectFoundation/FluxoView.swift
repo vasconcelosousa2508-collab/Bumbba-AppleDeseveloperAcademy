76c0d44bec698e61d523aed27e599db28c49febe
Clara <vasconcelosousa2508@gmail.com>
2026-06-19 22:24:39 -0300

    fluxo de leitura completo, inclui atividades e trechos

---
+++ FinalProjectFoundation/FluxoView.swift
@@ -0,0 +1,18 @@
+//
+//  FluxoView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 19/06/26.
+//
+
+import SwiftUI
+
+struct FluxoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FluxoView()
+}
