a256caeafdc10a48300f792c72c75db4157e78ff
Clara <vasconcelosousa2508@gmail.com>
2026-06-21 12:58:15 -0300

    banco de dados no arquivo principal

--- FinalProjectFoundation/FinalProjectFoundationApp.swift
+++ FinalProjectFoundation/FinalProjectFoundationApp.swift
@@ -4,31 +4,26 @@
 //
 //  Created by Beatriz Leonel on 28/05/26.
 //
-
 import SwiftUI
+import SwiftData
+import SwiftDataSQLite
 
 @main
 struct FinalProjectFoundationApp: App {
     var body: some Scene {
         WindowGroup {
             MainView()
+                .modelContainer(
+                    for: [
+                        Crianca.self, Responsavel.self, Avatar.self,
+                        Livro.self, LivroVersaoNivel.self, ConteudoLinha.self,
+                        Trecho.self, Atividade.self, AtividadeMultiplaEscolha.self
+                    ],
+                    // 💡 IMPORTANTE: Mude de 'true' para 'false'.
+                    // Se deixar 'true' (inMemory), ele ignora o seu arquivo SQLite e cria um banco limpo toda vez.
+                    inMemory: false,
+                    sqliteDatabasePath: Bundle.main.path(forResource: "db", ofType: "sqlite")!
+                )
         }
     }
 }
-
-//import SwiftUI
-//import SwiftData
-//
-//@main
-//struct SeuApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            BibliotecaView()
-//        }
-//        // Isso inicializa o arquivo do banco de dados SQLite para o modelo Livro
-//        .modelContainer(for: Livro.self)
-//    }
-//}
-//
-
-
